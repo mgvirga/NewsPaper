0d1b6c960b90b8a87a70264e1195bd5bfd078879
mgvirga112 <mgvirga@gmail.com>
2020-04-21 18:45:35 -0400

    John Changes

--- server.js
+++ server.js
@@ -3,13 +3,15 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
 const mongoose = require("mongoose");
+const path = require('path');
 const request = require('request');
+const PORT = 3000;
+const db = mongoose.connect("mongodb://localhost:27017/groceryManager", { useNewUrlParser: true, useUnifiedTopology: true })
 
-const db = mongoose.connect("mongodb://localhost:27017/groceryManager", { useNewUrlParser: true })
-
-const city = "alpharetta"
+const city = "alpharetta";
 
 const weatherUrl = "http://api.openweathermap.org/data/2.5/forecast/daily?q=" + city + "&mode=json&units=metric&cnt=5&appid=fbf712a5a83d7305c3cda4ca8fe7ef29";
+
 
 //defining js file for routing
 const login = require("./routes/index");
@@ -26,7 +28,6 @@ app.use(bodyParser.json({ type : "application/json" }));
 app.use(bodyParser.text({ type : "text/html" }));
 app.use(bodyParser.urlencoded({ extended : true }));
 
-
 //defining ejs files for routing
 app.use("/", login);
 app.use("/addItem", addItem);
@@ -36,9 +37,44 @@ app.use("/showItem", showItem);
 app.use("/weather", showWeather);
 app.use("/submitQuery", submitQuery);
 
-app.listen(3000, ()=>{
-    console.log("Server Started...")
+// import http from 'http';
+const http = require('http');
+
+let io = require('socket.io');
+
+const server = http.createServer(app).listen(PORT, () => {
+        console.log("server started at port: " + PORT);
+    })
+
+// Set up socket.io
+io = require('socket.io').listen(server);
+
+// Handle socket traffic
+io.sockets.on('connection',  (socket) => {
+ 
+    var list = io.sockets.sockets;
+    var users = Object.keys(list);
+   
+    socket.on('nick', (nick) => {
+        socket.nickname = nick;
+        socket.emit('userloggedin', users);
+    });
+
+    // Relay chat data to all clients
+    socket.on('chat', (data) => {
+            let payload = {
+                message: data.message,
+                nick: socket.nickname,
+            };
+
+            socket.emit('chat',payload);
+            socket.broadcast.emit('chat', payload);
+
+    });
 });
+
+
+
 
 
 //This is the bulk of hte backend pulling from mongoose
