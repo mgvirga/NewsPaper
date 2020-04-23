8d3e6dd686f767ca73d4561d949bc0bfd68d14e7
Danielle Jeter <d.jeter@tcs.com>
2020-04-23 12:39:52 -0400

    Danielle removed authentication from home

--- routes/home.js
+++ routes/home.js
@@ -4,37 +4,15 @@ const Router = express.Router();
 const UserModel = require("../model/user");
 const jwt = require('jsonwebtoken');
 const config = require('../config.js');
+const NewsModel = require("../model/news");
 
 // get home page
 Router.get("/", (req, res)=>{
-    // get token
-    var token = localStorage.getItem('authtoken')
-    if (!token) {
-        res.redirect('/')
-    }
-    // verify token
-    jwt.verify(token, config.secret, function(err, decoded) {
-    if (err) {
-        res.redirect('/')
-    };
-       UserModel.findById(decoded.id, { password: 0 }, function (err, user) {
-              if (err) {res.redirect('/')}
-              if (!user) {res.redirect('/')}
-              if(user.accountType === true )
-              {
-                NewsModel.find({}).then((docs)=>{
-                    console.log(docs);
-                    res.render("home", { posts : docs });
-                })
-              }
-              else
-              {
-                     res.redirect('/')
-              }
-       });
-    });
+    NewsModel.find({}).then((docs)=>{
+        console.log(docs);
+        res.render("home", { posts : docs });
+    })
 })
-const NewsModel = require("../model/news");
 
 
 module.exports = Router;
